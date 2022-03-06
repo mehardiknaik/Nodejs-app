c29d4203345e20d7ec2e972b1b2b0bd27572695b
Hardik Naik <hardiknaik7444@gmail.com>
2022-03-06 10:11:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 import express from "express";
-import bodyParser from "body-parser";
 import cors from "cors";
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
-app.use(bodyParser.json());
-
-app.get("/", (req, res) => res.send("Server is Working...!"));
-app.all("*", (req, res) => res.send("Route not found"));
+app.use(express.json());
+app.use(express.text());
+app.use(express.urlencoded({ extended: true }));
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+app.get("/", (req, res) => res.send("Server is Working...!"));
+app.post("/", (req, res) => res.send(req.body));
+app.all("*", (req, res) => res.send("Route not found"));
