3efaa24abed234a4fb2ebc7495005bb38faede5a
Hardik Naik <hardiknaik7444@gmail.com>
2022-07-14 16:43:06 +0530

    env

--- src/server.js
+++ src/server.js
@@ -3,8 +3,11 @@ import express from "express";
 import cors from 'cors';
 // Importing source files
 import routes from "./routes/main.routes";
+//Importing dotenv
+import dotenv from "dotenv"
 // consts
 const app = express();
+dotenv.config()
 const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(
